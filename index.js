d1978755c17f0654689c1a152456902026cbb8a6
toyjack <toyjack@gmail.com>
2018-06-19 15:04:19 +0900

    ほぼ完成

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let book;
 let data;
 let sheetNum = 0;
 let fieldNum = 8;
-let tags=['<jion>','<wakun>','<kanbun>']
+let tags = ['<jion>', '<wakun>', '<kanbun>']
 prog
   .version('0.0.1')
   .description('A cli for tagzuke (in development).')
@@ -27,16 +27,16 @@ prog
     data = XLSX.utils.sheet_to_json(sheet);
     console.log(Object.keys(data[0]))
     console.log('処理開始')
-    for (let i in data){
-      let defs= data[i].defs;
-      if(defs!=null){
+    for (let i in data) {
+      let defs = data[i].defs;
+      if (defs != null) {
         defs = defs.split(serprator);
-        for(let j in defs){
+        for (let j in defs) {
           defs[j] = check_def(defs[j]);
         }
         // defs=Array.from(new Set(defs.sort()))
-        data[i].defs=defs.join(serprator)
-        
+        data[i].defs = defs.join(serprator)
+
       }
     }
     logger.warn('saving file to out.xls')
@@ -51,7 +51,7 @@ function open_excelFile(excelFile) {
 }
 
 function save_file() {
-  
+
   let ws = XLSX.utils.json_to_sheet(data);
   let wb = {
     SheetNames: [],
@@ -71,39 +71,66 @@ function check_def(def) {
     return def
   }
   let rules = [{
-      tag: '<etc>',
-      regex: '同|同訓|不詳'
+      tag: '<jion>',
+      regex: '[平上去入]声',
+      attr: ''
     },
     {
       tag: '<jion>',
-      regex: '(.*反)|(音.+)|(.+音)'
+      regex: '^[上下]?[又亦呉和俗正]?[一二三四]?(又)?音',
+      attr: ''
+    },
+    {
+      tag: '<jion>',
+      regex: '音$',
+      attr: ''
+    },
+    {
+      tag: '<jion>',
+      regex: '[反切]$',
+      attr: ''
+    },
+    {
+      tag: '<jion>',
+      regex: '^[和禾].',
+      attr: '' //呉音
     },
     {
       tag: '<wakun>',
-      regex: '\\p{Katakana}.*'
+      regex: '\\p{Katakana}.*',
+      attr: ''
     },
     {
       tag: '<jitai>',
-      regex: '.*[俗|正|或|古].*'
+      regex: '[俗正或古篆今通作]',
+      attr: ''
     },
     {
       tag: '<kanbun>',
-      regex: '也$'
+      regex: '也$',
+      attr: ''
+    },
+    {
+      tag: '<etc>',
+      regex: '同|同訓|不詳|又',
+      attr: ''
     },
     {
       tag: '<kanbun>',
-      regex: '\\p{Han}.'
+      regex: '\\p{Han}|ー',
+      attr: ''
     },
   ]
   for (let i in rules) {
-    def=def.replace(/\([^)]+\)/,'');
-    def=def.replace(/（[^）]+）/,'');
+    def = def.replace(/\([^)]+\)/, '');
+    def = def.replace(/（[^）]+）/, '');
     if (XRegExp(rules[i].regex).test(def)) {
       let endTag = rules[i].tag.replace(/</, '</')
       return rules[i].tag + def + endTag
       // return rules[i].tag
     }
   }
-  return '<unknown>'+def+'</unknown>'
-
+  if(def != '') {
+    return '<unknown>' + def + '</unknown>'
+  }
 }
